4e2ce0963c38e395edae52fc960b08a08363d5b0
GRaguzzoni <guilherme.raguzzoni@gmail.com>
2021-06-28 23:46:40 -0300

    routes fixes

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -24,6 +24,8 @@ export default function PostPreview({post}: PostPreviewProps) {
   useEffect(() => {
     if (session?.activeSubscription) {
       router.push(`/posts/${post.slug}`)
+
+      return
     }
   }, [session])
 
